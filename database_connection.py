85f33fca49f580dac4d8ff3f28fc5c70f9e33beb
Bart-v-Lee <bwhvanderlee@gmail.com>
2018-02-16 16:59:27 +0100

    Changes to code after feedback
    Further changes to the code after feedback

---
+++ database_connection.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 16 15:39:57 2018
+
+@author: Bart
+"""
+
+class Database:
+    
+    def __init__():
+        pass
+        
+    def RetrieveMaterial():
+        pass
+    
+    def RetrieveBoundaryConditions():
+        pass
+    
+    def RetrieveSeedDesigns():
+        pass
